528e6bd6d224fc8454a19bad4bf21b7086c8e46b
CAROLINA7891 <169111738+CAROLINA7891@users.noreply.github.com>
2024-05-07 21:54:24 -0400

    Add files via upload

---
+++ dilevery de sushi.py
@@ -0,0 +1,20 @@
+# dilevery de sushi
+# 1 pikachu roll $ 4500
+# otaku roll $ 5000
+# pulpo venenoso roll $ 5200
+# anguila electrica roll $ 4800
+
+import os
+
+pikachu_roll= 4500
+otaku_roll= 5000
+pulpo_venenosa_roll=5200
+anguila_eléctrica_roll=4800
+
+while True:
+    print(" ingrese su pedido de sushi")
+    pikachu_roll= int(input("ingrese la cantidad de su pedido"))
+    pikachu_roll==1
+    otaku_roll=int(input("ingresa la cantidad de su pedido"))
+    otaku_roll=2
+
